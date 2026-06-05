25105d32a319e37c6adce3817db4f80f79bc22e5
albashaalbasha4454-sudo <albasha.albasha4454@gmail.com>
2026-06-05 13:49:14 +0300

    Restore Firebase provider with standard login

--- components/FirebaseProvider.tsx
+++ components/FirebaseProvider.tsx
@@ -6,7 +6,3 @@ import { simpleHash } from '../utils/authUtils';
 interface FirebaseContextType {
     user: FirebaseUser | null;
     currentUser: User | null;
-    loading: boolean;
-    isAdmin: boolean;
-    error: string | null;
-    loginWithCredentials: (username: string
