9b5da59d26d23bf76becf6aa26aab38af5a98d1b
albashaalbasha4454-sudo <albasha.albasha4454@gmail.com>
2026-06-05 11:42:33 +0300

    Remove incomplete public portal component

--- components/PublicLibraryPortal.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-type Props = { onLogin: () => Promise<void> };
-
-export default function PublicLibraryPortal({ onLogin }: Props) {
-  return <div dir="rtl" className="min-h-screen flex items-center justify-center bg-slate-950 text-white p-6"><main className="max-w-3xl rounded-3xl border border-white/10 bg-white/10 p-8"><h1 className="text-4xl
